15ff0d5b52763b056a4002918a107ee601cfe5d2
Francois-Rene <leautier.fr@gmail.com>
2023-02-09 14:34:50 -0500

    fixes favorites display

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,14 @@
+require "pry"
+
 class FavoritesController < ApplicationController
+  def index
+    favorites = Favorite.all
+    if current_user
+      favorites = Favorite.where(user_id: current_user.id)
+    end
+    render json: favorites.as_json
+  end
+
   def create
     favorite = Favorite.new(
       user_id: current_user.id,
@@ -12,10 +22,5 @@ class FavoritesController < ApplicationController
     favorite = Favorite.find_by(id: params[:id])
     favorite.destroy
     render json: { message: "Unfavorited" }
-  end
-
-  def index
-    favorites = Favorite.all
-    render json: favorites.as_json
   end
 end
